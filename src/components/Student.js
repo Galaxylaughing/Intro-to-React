803b12fe575bae70d7bd1961fae12df3f8279542
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-22 10:19:18 -0800

    moved selectStyles into buildStyles method

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Student.css';
 
 const Student = (props) => {
-  const selectStyles = props.person.class.toLowerCase() 
+
+  const buildStyles = () => {
+    const selectStyles = props.person.class.toLowerCase() 
       + ' '
       + ((props.person.present) ? 'present' : 'absent');
 
+    return selectStyles;
+  }
+
   // component functions always return JSX
   return (
-    <section className={selectStyles}>
+    <section className={buildStyles()}>
       <h3>{props.person.fullName}</h3>
       <p>{props.person.email}</p>
     </section>
