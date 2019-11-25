cee23933f9388bf1bd41861b4c36c7eeeee10ed8
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-25 10:47:02 -0800

    changed function App to class App

--- src/App.js
+++ src/App.js
@@ -4,40 +4,79 @@ import './App.css';
 import StudentCollection from './components/StudentCollection';
 import ComponentA from './components/ComponentA';
 
-const name = 'Sabrina';
-const students = [
-  { fullName: "Ada",        email: "ada@lovelace.com",  class: "Leaves",    present: false },
-  { fullName: "Katherine",  email: "kat@nasa.gov",      class: "Branches",  present: false },
-  { fullName: "Devin",      email: "dmoney@g.mail",     class: "Leaves",    present: true },
-  { fullName: "Becca",      email: "bmoney@g.mail",     class: "Leaves",    present: true },
-  { fullName: "Dee",        email: "dee@dee.dee",       class: "Branches",  present: true },
-  { fullName: "Tonks",                                                      present: true }
-];
+// const name = 'Sabrina';
+// const students = [
+//   { fullName: "Ada",        email: "ada@lovelace.com",  class: "Leaves",    present: false },
+//   { fullName: "Katherine",  email: "kat@nasa.gov",      class: "Branches",  present: false },
+//   { fullName: "Devin",      email: "dmoney@g.mail",     class: "Leaves",    present: true },
+//   { fullName: "Becca",      email: "bmoney@g.mail",     class: "Leaves",    present: true },
+//   { fullName: "Dee",        email: "dee@dee.dee",       class: "Branches",  present: true },
+//   { fullName: "Tonks",                                                      present: true }
+// ];
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <StudentCollection people={students} />
+//         <ComponentA ComponentB />
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React!
+//         </a>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <StudentCollection people={students} />
-        <ComponentA ComponentB />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        
-        <p>{name}'s kitten:</p>
-        <img src="http://placekitten.com/400/300" alt="kitten" />
-      </header>
-    </div>
-  );
+//         <p>{name}'s kitten:</p>
+//         <img src="http://placekitten.com/400/300" alt="kitten" />
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.name = 'Sabrina';
+    this.students = [
+      { fullName: "Ada",        email: "ada@lovelace.com",  class: "Leaves",    present: false },
+      { fullName: "Katherine",  email: "kat@nasa.gov",      class: "Branches",  present: false },
+      { fullName: "Devin",      email: "dmoney@g.mail",     class: "Leaves",    present: true },
+      { fullName: "Becca",      email: "bmoney@g.mail",     class: "Leaves",    present: true },
+      { fullName: "Dee",        email: "dee@dee.dee",       class: "Branches",  present: true },
+      { fullName: "Tonks",                                                      present: true },
+    ];
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <StudentCollection people={this.students} />
+          <ComponentA ComponentB />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React!
+          </a>
+    
+          <p>{this.name}'s kitten:</p>
+          <img src="http://placekitten.com/400/300" alt="kitten" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
