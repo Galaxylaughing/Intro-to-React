42935185526b3d671dfff5124fd6816d9a8df948
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-25 11:15:06 -0800

    changed function StudentCollection to class component

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import Student from './Student';
 
-const StudentCollection = (props) => {
+// const StudentCollection = (props) => {
+//   // turn array of student object into an array of JSX list items
+//   const studentComponents = props.people.map((student, i) => {
+//     return (
+//       <li key={i}>
+//         <Student fullName={student.fullName} email={student.email} class={student.class} present={student.present} />
+//       </li>
+//     );
+//   });
+
+//   // return all JSX list items in an unordered list
+//   return (
+//     <ul className="student-collection">
+//       {studentComponents}
+//     </ul>
+//   );
+// };
+
+class StudentCollection extends React.Component {
+
   // turn array of student object into an array of JSX list items
-  const studentComponents = props.people.map((student, i) => {
-    return (
-      <li key={i}>
-        <Student fullName={student.fullName} email={student.email} class={student.class} present={student.present} />
-      </li>
-    );
-  });
+  buildComponents () {
+    return this.props.people.map( ( student, i ) => {
+      return (
+        <li key={ i }>
+          <Student fullName={ student.fullName } email={ student.email } class={ student.class } present={ student.present } />
+        </li>
+      );
+    } );
+  }
 
   // return all JSX list items in an unordered list
-  return (
-    <ul className="student-collection">
-      {studentComponents}
-    </ul>
-  );
+  render () {
+    return (
+      <ul className="student-collection">
+        { this.buildComponents() }
+      </ul>
+    );
+  }
 };
 
 export default StudentCollection;
