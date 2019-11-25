cf1ae88af11e8dffee7f83c18a5d40ef16806b9d
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-25 11:04:45 -0800

    Removed old bits from StudentCollection

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -2,20 +2,11 @@ import React from 'react';
 import Student from './Student';
 
 const StudentCollection = (props) => {
-  // const students = [
-  //   { fullName: "Ada",        email: "ada@lovelace.com" },
-  //   { fullName: "Katherine",  email: "kat@nasa.gov" },
-  //   { fullName: "Devin",      email: "dmoney@g.mail" },
-  //   { fullName: "Becca",      email: "bmoney@g.mail" },
-  //   { fullName: "Dee",        email: "dee@dee.dee" }
-  // ];
-
   // turn array of student object into an array of JSX list items
   const studentComponents = props.people.map((student, i) => {
     return (
       <li key={i}>
         <Student fullName={student.fullName} email={student.email} class={student.class} present={student.present} />
-        {/* <Student fullName={student.fullName} email={student.email} /> */}
       </li>
     );
   });
