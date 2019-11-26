afa169aec6e9d1a0a8c228253543ce491513c2f9
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-26 09:36:37 -0800

    changed student back to functional component

--- src/components/Student.js
+++ src/components/Student.js
@@ -21,34 +21,68 @@ import PropTypes from 'prop-types';
 //   );
 // };
 
-class Student extends React.Component {
-  constructor(props) {
-    super(props); // creates this.props = props
-    this.state = {
-      present: this.props.present,
-    }
-  }
 
-  buildStyles() {
-    const selectStyles = ((this.props.class) ? this.props.class.toLowerCase() : '')
+
+// class Student extends React.Component {
+//   constructor(props) {
+//     super(props); // creates this.props = props
+//     this.state = {
+//       present: this.props.present,
+//     }
+//   }
+
+//   buildStyles() {
+//     const selectStyles = ((this.props.class) ? this.props.class.toLowerCase() : '')
+//       + ' '
+//       + ((this.props.present) ? 'present' : 'absent');
+
+//     return selectStyles;
+//   }
+
+//   render() {
+//     return (
+//       <section className={this.buildStyles()}>
+//         <h3>{this.props.fullName}</h3>
+//         <p>{this.props.email}</p>
+//       </section>
+//     );
+//   }
+// };
+
+// Student.propTypes = {
+//   // person: PropTypes.object.isRequired,
+//   fullName: PropTypes.string.isRequired,
+//   class: PropTypes.string,
+//   email: PropTypes.string,
+//   present: PropTypes.bool.isRequired
+// };
+
+// // // You can also set default values for some or all of your props with a defaultProps static attribute:
+// Student.defaultProps = {
+//   class: 'no-class',
+//   email: 'no email on file',
+// }
+
+
+
+const Student = (props) => {
+  const buildStyles = () => {
+    const selectStyles = ((props.class) ? props.class.toLowerCase() : '')
       + ' '
-      + ((this.props.present) ? 'present' : 'absent');
-
+      + ((props.present) ? 'present' : 'absent');
+      
     return selectStyles;
   }
 
-  render() {
-    return (
-      <section className={this.buildStyles()}>
-        <h3>{this.props.fullName}</h3>
-        <p>{this.props.email}</p>
-      </section>
-    );
-  }
+  return (
+    <section className={buildStyles()}>
+      <h3>{props.fullName}</h3>
+      <p>{props.email}</p>
+    </section>
+  );
 };
 
 Student.propTypes = {
-  // person: PropTypes.object.isRequired,
   fullName: PropTypes.string.isRequired,
   class: PropTypes.string,
   email: PropTypes.string,
@@ -60,5 +94,6 @@ Student.defaultProps = {
   class: 'no-class',
   email: 'no email on file',
 }
+
 
 export default Student;
